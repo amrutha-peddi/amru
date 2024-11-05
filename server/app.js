2197daf4228ccca3e8dea67f7bd596d4e7e0a878
amrutha-peddi <amruthapeddi6@gmail.com>
2024-11-05 11:25:56 +0530

    Update app.js
    added default roter

--- server/app.js
+++ server/app.js
@@ -25,6 +25,10 @@ app.use(express.json());
 
 //connect mongodb
 connectDB();
+//defalult route
+app.get("/",(req,res)=>{
+  res.json("Welcome to Student Portal")
+})
 
 //routes
 const userRoutes = require("./routes/User");
